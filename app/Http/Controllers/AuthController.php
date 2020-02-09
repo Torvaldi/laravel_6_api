25192c947b50c51605c4ed7af3a071491d919366
falia15 <math.1510@hotmail.fr>
2020-02-09 12:25:44 +0100

    make validator works with json data

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,15 +32,21 @@ class AuthController extends Controller
     {
         // check password confirmation
         if($request->input('password') !== $request->input('password_confirmation')){
-            return response()->json(['error' => 'Password and password confirmation does not match']);
+            return response()->json(['error' => ['Password and password confirmation does not match']], 400);
         }
         
         // validate request
-        $request->validate([
+        $rules = [
             'username' => 'required|string|min:1|max:10',
             'password' => 'required|string|min:4|max:10',
             'email' => 'required|string|min:4'
-        ]);
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()){
+            return response()->json(["error" => $validator->errors()->all()], 400);
+        }
 
         $username = $request->input('username');
         // encode password
@@ -51,14 +57,14 @@ class AuthController extends Controller
         $usernameExist = $this->userRepository->getUserByUsername($username);
         // user exist
         if ($usernameExist !== null) {
-            return response()->json(['error' => 'This username is already taken']);
+            return response()->json(['error' => ['This username is already taken']], 400);
         }
 
         // check if mail exist
         $mailExist = $this->userRepository->getUserByEmail($email);
         // mail exist
         if ($mailExist !== null) {
-            return response()->json(['error' => 'This email is already taken']);
+            return response()->json(['error' => ['This email is already taken']], 400);
         }
 
         // save user to database
@@ -68,7 +74,7 @@ class AuthController extends Controller
         $user->email= $email;
         $user->save();
 
-        return response()->json($user);
+        return response()->json($user, 200);
     }
 
     /**
@@ -78,29 +84,35 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         // validate request
-        $request->validate([
+        $rules = [
             'username' => 'required',
             'password' => 'required',
-        ]);
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails()){
+            return response()->json(["error" => $validator->errors()->all()], 400);
+        }
 
         // get user from database
         $user = $this->userRepository->getUserByUsername($request->input('username'));
     
         // check user exist
         if($user === null){
-            return response()->json(['error' => 'This username does not exist']);
+            return response()->json(['error' => ['This username does not exist']], 400);
         }
 
         // check password
         if (Hash::check($request->input('password'), $user->password) === false) {
-            return response()->json(['error' => 'Password incorrect']);
+            return response()->json(['error' => ['Password incorrect']], 400);
         }
 
         // genere token
         $token = $this->jwt->genereToken($user->id, $user->username, $user->password);
 
         // return token
-        return response()->json(['token' => $token]);
+        return response()->json(['token' => $token], 200);
     }
 
     /**
@@ -123,7 +135,7 @@ class AuthController extends Controller
         
         // check if new password is the same as the confirmation password
         if ($request->newPassword != $request->confirmNewPassword){
-            return response()->json(['error' => 'Password and password confirmation does not match']);
+            return response()->json(['error' => ['Password and password confirmation does not match']], 400);
         }
 
         // retrive user with token
@@ -132,7 +144,7 @@ class AuthController extends Controller
 
         // check if the old password given by the user is the same as the current password
         if (Hash::check($request->input('oldPassword'), $currentPassword) === false){
-            return response()->json(['error' => 'You password is incorrect']);
+            return response()->json(['error' => ['Your password is incorrect']], 400);
         }
 
         // hash the new password
@@ -143,7 +155,7 @@ class AuthController extends Controller
         $user->password = $passwordHash;
         $user->save();
 
-        return response()->json($user);
+        return response()->json($user, 200);
     }
 
 }
