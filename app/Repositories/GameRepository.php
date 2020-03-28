adfa335055cc56fdf622458ff528ce551ba2beb6
Xanders07 <darken2552@gmail.com>
2020-03-28 17:10:48 +0100

    sorry

--- app/Repositories/GameRepository.php
+++ app/Repositories/GameRepository.php
@@ -17,8 +17,7 @@ class GameRepository {
         $sql = "SELECT game_user.user_id
         FROM games
         JOIN game_user ON game_user.game_id = games.id
-        WHERE games.status != 3
-        AND game_user.user_id == ?";
+        WHERE games.status != 3 AND game_user.user_id = ?";
 
         $totalUserCurrentgames = DB::select($sql, [$userId]);
 
